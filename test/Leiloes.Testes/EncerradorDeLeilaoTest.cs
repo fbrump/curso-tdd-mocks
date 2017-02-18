7ea618fa5a3b90d161d8aac75d70b69abd8ddec0
Felipe Brum <fbrump@gmail.com>
2017-02-18 00:50:25 -0200

    Added other method for teste when date of week is today, can not fineshed

--- test/Leiloes.Testes/EncerradorDeLeilaoTest.cs
+++ test/Leiloes.Testes/EncerradorDeLeilaoTest.cs
@@ -42,6 +42,36 @@ namespace Leiloes.Testes
             Assert.True(leilao1.encerrado);
             Assert.True(leilao2.encerrado);
         }
+
+        [Fact]
+        public void Should_fineshed_aoction_that_started_today()
+        {
+            //Given
+            DateTime diaDaSemanaPassada = DateTime.Now;
+            
+            Leilao leilao1 = new Leilao("Tv de plasma");
+            leilao1.naData(diaDaSemanaPassada);
+
+            Leilao leilao2 = new Leilao("Playstation");
+            leilao2.naData(diaDaSemanaPassada);
+
+            List<Leilao> ListaDeLeiloes = new List<Leilao>();
+            ListaDeLeiloes.Add(leilao1);
+            ListaDeLeiloes.Add(leilao2);
+
+            var dao = new Mock<LeilaoDaoFalso>();
+
+            dao.Setup(d => d.Correntes())
+                .Returns(ListaDeLeiloes);
+            //When
+            EncerradorDeLeilao encerrador = new EncerradorDeLeilao(dao.Object);
+            encerrador.Encerra();
+            
+            //Then
+            Assert.Equal(0, encerrador.Total);
+            Assert.False(leilao1.encerrado);
+            Assert.False(leilao2.encerrado);
+        }
         
     }
 }
