a1a895281b256e2785fb93b4a6ab467ffaa34ec2
Felipe Brum <fbrump@gmail.com>
2017-02-17 23:56:03 -0200

    Implementation for first unit test

---
+++ test/Leiloes.Testes/EncerradorDeLeilaoTest.cs
@@ -0,0 +1,36 @@
+namespace Leiloes.Testes
+{
+    using System;
+    using System.Linq;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Xunit;
+    using Leiloes.Dominio;
+    using Leiloes.Servico;
+
+    public class EncerradorDeLeilaoTest
+    {
+        [Fact]
+        public void Should_fineshed_aoction_that_started_one_week()
+        {
+            //Given
+            DateTime diaDaSemanaPassada = new DateTime(1999, 05, 05);
+            
+            Leilao leilao1 = new Leilao("Tv de plasma");
+            leilao1.naData(diaDaSemanaPassada);
+
+            Leilao leilao2 = new Leilao("Playstation");
+            leilao2.naData(diaDaSemanaPassada);
+
+            //When
+            EncerradorDeLeilao encerrador = new EncerradorDeLeilao();
+            encerrador.Encerra();
+            
+            //Then
+            Assert.Equal(2, encerrador.Total);
+            Assert.True(leilao1.encerra());
+            Assert.True(leilao2.encerra());
+        }
+        
+    }
+}
