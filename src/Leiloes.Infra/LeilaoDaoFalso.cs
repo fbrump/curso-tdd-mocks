a14e98f65f19ec152f363263d30556618e1cf2d2
Felipe Brum <fbrump@gmail.com>
2017-02-17 23:20:37 -0200

    Added business in LeilaoDaoFalse.cs with fake datas

--- src/Leiloes.Infra/LeilaoDaoFalso.cs
+++ src/Leiloes.Infra/LeilaoDaoFalso.cs
@@ -8,14 +8,35 @@ namespace Leiloes.Infra
 
     public class LeilaoDaoFalso
     {
-        public List<Leilao> Correntes()
+        private static List<Leilao> leiloes = new List<Leilao>();
+
+        public void Salva(Leilao leilao)
         {
-            return new List<Leilao>();
+            leiloes.Add(leilao);
         }
 
-        public void Atualiza(Leilao leilao)
+        public virtual List<Leilao> Encerrados()
         {
+            List<Leilao> filtrados = new List<Leilao>();
+            foreach (var l in leiloes)
+            {
+                if (l.encerrado) filtrados.Add(l);
+            }
 
+            return filtrados;
         }
+
+        public virtual List<Leilao> Correntes()
+        {
+            List<Leilao> correntes = new List<Leilao>();
+            foreach (var l in leiloes)
+            {
+                if (!l.encerrado) correntes.Add(l);
+            }
+
+            return correntes;
+        }
+
+        public virtual void Atualiza(Leilao leilao) { }
     }
 }
